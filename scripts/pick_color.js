1a47a64a1fd4805a0d36f162b74f788a9e01b5eb
ED <73085168+ef1452@users.noreply.github.com>
2023-12-18 11:00:17 -0500

    updated pick_color and index logic

--- scripts/pick_color.js
+++ scripts/pick_color.js
@@ -1,15 +1,24 @@
 var colors = ['blue', 'green', 'orange', 'red'];
 
-function pickColor() {
-  var color = colors.shift();
-  colors.push(color);
+document.addEventListener('DOMContentLoaded', function () {
+  // Initial color
+  var color = colors[0];
   
+  // Set up the color-demo div
   var colorDemo = document.getElementById('color-demo');
   if (colorDemo) {
     colorDemo.style.color = color;
-    colorDemo.innerHTML = 'Color Swap Demo';
-  }
-}
+    colorDemo.innerHTML = 'I made this';
 
-// Trigger the pickColor function on page load
-pickColor();
+    // Define the htmx handler for the /colors endpoint
+    htmx.on('htmx:afterRequest', function (event) {
+    
+      if (event.detail.xhr.responseURL.endsWith('/colors')) {
+        // Update the color after the request
+        color = colors.shift();
+        colors.push(color);
+        colorDemo.style.color = color;
+      }
+    });
+  }
+});
