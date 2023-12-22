78752bdcf7be1d90f4a0a9682fe3bd3d2d5c28b2
ED <73085168+ef1452@users.noreply.github.com>
2023-12-21 21:40:23 -0500

    paint_program.js added also fixed images in index

--- scripts/pick_color.js
+++ scripts/pick_color.js
@@ -1,24 +1,9 @@
+
 var colors = ['blue', 'green', 'orange', 'red'];
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Initial color
-  var color = colors[0];
-  
-  // Set up the color-demo div
-  var colorDemo = document.getElementById('color-demo');
-  if (colorDemo) {
-    colorDemo.style.color = color;
-    colorDemo.innerHTML = 'I made this';
-
-    // Define the htmx handler for the /colors endpoint
-    htmx.on('htmx:afterRequest', function (event) {
-    
-      if (event.detail.xhr.responseURL.endsWith('/colors')) {
-        // Update the color after the request
-        color = colors.shift();
-        colors.push(color);
-        colorDemo.style.color = color;
-      }
-    });
-  }
-});
+  var color = colors.shift();
+  colors.push(color);
+  return '<div id="color-demo" hx-get="scripts/pick_colors.js" hx-swap="outerHTML" class="smooth" hx-trigger="every 1s" style="color:' + color + '">\n'+
+         '  Color Swap Demo\n'+
+         '</div>\n'
+;
